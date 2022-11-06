4336625ebc6d86c78c2801d08b44dac98dd2ac19
anlenczo <lenczowska.a@gmail.com>
2022-11-06 16:36:52 +0100

    dodano nowe pliki

---
+++ src/ClassTester.java
@@ -0,0 +1,24 @@
+/*
+public class ClassTester {
+    int i; //zmienna egzemplarza (pole klasy - definiowane zaraz na górze)
+    static int j = 2; //zmienna klasowa (pole statyczne)
+
+    public int change() {
+        int k = 7;      //zmienna lokalna, zdefiniowana w ciele metody
+        return i + (k * j)
+    }
+    public static void main(String[] dowolnaNazwa) {
+
+        ClassTester ct = new ClassTester(); //zmienna lokalna
+        ct.i = 4;
+        ClassTester ct2 = new ClassTester();
+        ct2.i = 9; //zmienna lokalna
+        System.out.println(ct.i);
+
+        System.out.println(j);
+
+    }
+}
+
+class A {}      //w jednym pliku może istnieć kilka klas, ale tylko jedna może być publiczna
+*/
---
+++ src/RangeLister.java
@@ -0,0 +1,24 @@
+public class RangeLister {
+    int[] makeRange(int lower, int upper) {
+        int [] range = new int[(upper - lower) + 1];
+        for (int i = 0; i < range.length; i++) {
+            range[i] = lower++;
+
+        }
+        return new int[1];
+
+    }
+
+    public static void main(String[] args) {
+        int[] range;
+        RangeLister rl = new RangeLister();
+        range = rl.makeRange(4, 13);
+
+        System.out.println("Tablica: [");
+        for (int i = 0; i < range.length; i++) {
+            System.out.print(range[i] + " ");
+        }
+        System.out.println("]");
+
+    }
+}
